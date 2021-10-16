c4b75bacddc97a910f63b0d0201ca2efc55c7644
mayuri43 <mayuriagrawal33@gmail.com>
2021-10-16 17:58:47 +0000

    2nd commit

--- test.sh
+++ test.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-aws ec2 describe-tags \
+aws configure
+AWS Access Key ID [None]: AKIAT2NBG2XIVC2NLOLH
+AWS Secret Access Key [None]: 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
+Default region name [None]: us-east-2
+Default output format [None]:
+
+
+cat output.txt > aws ec2 describe-tags \
 
